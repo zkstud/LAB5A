8298d03ad820c8430e4b8a3a8d1d14a3e2ca84aa
ZAKHAR <37652@studentwsei.pl>
2022-05-21 20:11:46 +0200

    regular expression do klasy Noblista

--- src/main/java/pl/lublin/wsei/java/Noblista.java
+++ src/main/java/pl/lublin/wsei/java/Noblista.java
@@ -6,19 +6,52 @@ import java.util.regex.Pattern;
 public class Noblista {
     public String firstname;
     public String surname;
-    public String year;
+    public int year;
     public String category;
     public String motivation;
     public String country;
 //    public String dataRow;
 
-    //TUTAJ REGEX
+    //TUTAJ KONSTRUKTOR Z REGEX
     public Noblista(String tekst) {
-        Pattern pat = Pattern.compile("");
+        Pattern pat = Pattern.compile("\\n\\d{3}\\,(\\w+|\\w+\\s\\w+)(\\.|\\,)");
         Matcher m = pat.matcher(tekst);
         if (m.find()) {
             firstname = m.group(1);
         } else firstname = "";
+
+        pat = Pattern.compile("\\,(\\w+|\\w+\\s\\w+|\\w+\\-\\w+)\\,\\d");
+        m = pat.matcher(tekst);
+        if (m.find()) {
+            surname = m.group(1);
+        } else surname = "";
+
+        pat = Pattern.compile("\\,(\\d...)\\,");
+        m = pat.matcher(tekst);
+        if (m.find()) {
+            year = Integer.parseInt(m.group(1));
+        } else year = 0;
+
+        pat = Pattern.compile("\\,(\\w+)\\,\\,\\d");
+        m = pat.matcher(tekst);
+        if (m.find()) {
+            category = m.group(1);
+        } else category = "";
+
+        pat = Pattern.compile("\\\"\\\"\\\"(.*)\\\"\\\"\\\"");
+        m = pat.matcher(tekst);
+        if (m.find()) {
+            motivation = m.group(1);
+        } else motivation = "";
+
+        pat = Pattern.compile("[^country]\\,(\\w+\\s\\w+|\\w+)\\n");
+        m = pat.matcher(tekst);
+        if (m.find()) {
+            country = m.group(1);
+        } else country = "";
     }
 
+    public void print() {
+        System.out.println(firstname + "\n" + surname + "\n" + year + "\n" + category + "\n" + motivation + "\n" + country + "\n\n");
+    }
 }
